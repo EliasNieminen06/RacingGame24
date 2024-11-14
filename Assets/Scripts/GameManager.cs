d86c33fd497e8f697e9a74fa1f69d9c50c06a0fa
EliasNieminen06 <143606682+EliasNieminen06@users.noreply.github.com>
2024-11-14 13:48:19 +0200

    update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,8 +2,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public int totalLaps = 3;
+    public FinishLine finishLine;
+    public float timer;
+
     private void Awake()
     {
         Application.targetFrameRate = 60;
+    }
+
+
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (finishLine.currentLap > totalLaps)
+        {
+            print("You finished!");
+        }
     }
 }
